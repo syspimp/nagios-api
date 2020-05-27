4838e7996990223193487f559c8bb97eefd2ab8f
David Taylor <dataylor@yogac940.tfound.org>
2020-05-27 11:45:07 -0400

    changing the FROM image

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/bin/start_nagios
-/opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log -q
+/usr/local/bin/start_nagios &
+/opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log &
