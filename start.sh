b263173602207ea93c4fbe91d844e4d602007a6d
David Taylor <dataylor@yogac940.tfound.org>
2020-05-27 11:54:09 -0400

    adding sleep to create status.dat ... throwing start nagios to background

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /usr/local/bin/start_nagios &
+sleep 60
 /opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log
