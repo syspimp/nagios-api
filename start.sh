500a953e63f907fefde6334704f954551f7a27a8
David Taylor <dataylor@yogac940.tfound.org>
2020-05-28 18:48:24 -0400

    forgot to invoke cron

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 /usr/local/bin/start_nagios &
 echo waiting 10 for nagios to start
 sleep 10
+cron
 KUBE_NAME=$(echo ${KUBERNETES_PORT_443_TCP_ADDR} | tr '.' '-')
 sed -i -e "s/xxxxxx:6443/${KUBERNETES_PORT_443_TCP_ADDR}/g" /opt/nagios-api/kubeconfig
 sed -i -e "s/yyyyyy:6443/${KUBE_NAME}/g" /opt/nagios-api/kubeconfig
