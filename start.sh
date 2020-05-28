77dad3746bc4c1e62734d7a39bdb05f8467ef7e3
David Taylor <dataylor@yogac940.tfound.org>
2020-05-28 19:11:01 -0400

    fixing order of operations

--- start.sh
+++ start.sh
@@ -3,7 +3,4 @@
 /usr/local/bin/start_nagios &
 echo waiting 10 for nagios to start
 sleep 10
-KUBE_NAME=$(echo ${KUBERNETES_PORT_443_TCP_ADDR} | tr '.' '-')
-sed -i -e "s/xxxxxx:6443/${KUBERNETES_PORT_443_TCP_ADDR}/g" /opt/nagios-api/kubeconfig
-sed -i -e "s/yyyyyy:6443/${KUBE_NAME}/g" /opt/nagios-api/kubeconfig
 /opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log
