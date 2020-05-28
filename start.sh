13a828b5a27c6905cdc8366f7054a76d1bb97fec
David Taylor <dataylor@yogac940.tfound.org>
2020-05-28 19:19:07 -0400

    fixing order of operations

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/opt/nagios-api/reconfig-nagios.sh
 /usr/local/bin/start_nagios &
 echo waiting 10 for nagios to start
 sleep 10
+/opt/nagios-api/reconfig-nagios.sh
+$(sleep 10 && curl http://localhost:8080/restart_nagios ) &
 /opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log
