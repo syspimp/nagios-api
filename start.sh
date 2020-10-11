a74ba4874e7f8330b266bcc398f8e4c3dae6e683
David Taylor <dataylor@yogac940.tfound.org>
2020-10-11 17:04:28 -0400

    updating

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 echo waiting 10 for nagios to start
 sleep 10
 /opt/nagios-api/reconfig-nagios.sh
-$(sleep 10 && curl http://localhost:8080/restart_nagios ) &
+echo $(sleep 10 && curl http://localhost:8080/restart_nagios ) &
 /opt/env/bin/python /opt/nagios-api/nagios-api -p 8080 -s /opt/nagios/var/status.dat -c /opt/nagios/var/rw/nagios.cmd -l /opt/nagios/var/nagios.log
